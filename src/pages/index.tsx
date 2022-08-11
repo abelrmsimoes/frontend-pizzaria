73475aa6f65f27e1ab7516f0d1f76b2dc8e805f3
Abel Simões <abelrmsimoes@gmail.com>
2022-08-11 12:04:11 -0300

    Iniciando tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+
+import logoImg from '../../public/logo.svg';
+import { Input } from '../components/ui/Input';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Pizza Station</h1>
-    </div>
+    <>
+      <Head>
+        <title>PizzaStation - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo PizzaStation" />
+
+        <div className={styles.login}>
+          <form>
+            <Input type="text" placeholder="Digite seu email" />
+            <Input type="password" placeholder="Digite sua senha" />
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
