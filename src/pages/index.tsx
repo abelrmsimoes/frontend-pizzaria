4c3ebbc50dbd4df5729639010244948be17d7f45
Abel Simões <abelrmsimoes@gmail.com>
2022-08-16 10:06:21 -0300

    Finalizando tela login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../../styles/home.module.scss';
 
 import logoImg from '../../public/logo.svg';
 import { Input } from '../components/ui/Input';
+import { Button } from '../components/ui/Button';
 
 export default function Home() {
   return (
@@ -18,7 +19,11 @@ export default function Home() {
           <form>
             <Input type="text" placeholder="Digite seu email" />
             <Input type="password" placeholder="Digite sua senha" />
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
           </form>
+          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
         </div>
       </div>
     </>
