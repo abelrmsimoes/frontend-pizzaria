9383fb756afecd2179e569afc25bcd14278a8de5
Abel Simões <abelrmsimoes@gmail.com>
2022-08-16 16:08:36 -0300

    Configurando api e fazendo login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,14 @@
 import { createContext, ReactNode, useState } from 'react';
+import { destroyCookie, setCookie, parseCookies } from 'nookies';
+import Router from 'next/router';
+
+import { api } from '../services/apiClient';
 
 type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
+  signOut: () => void;
 };
 
 type UserProps = {
@@ -23,16 +28,49 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  try {
+    destroyCookie(undefined, '@nextauth.token');
+    Router.push('/');
+  } catch {
+    console.log('Erro ao deslogar');
+  }
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user;
 
-  async function signIn() {
-    alert('Clicou no login!');
+  async function signIn({ email, password }: SignInProps) {
+    try {
+      const response = await api.post('/session', {
+        email,
+        password,
+      });
+
+      const { id, name, token } = response.data;
+
+      setCookie(undefined, '@nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: '/',
+      });
+
+      setUser({
+        id,
+        name,
+        email,
+      });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      Router.push('/dashboard');
+    } catch (err) {
+      console.error('Erro ao acesar', err);
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, FormEvent } from 'react';
+import { useContext, FormEvent, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -14,12 +14,16 @@ import { AuthContext } from '../contexts/AuthContext';
 export default function Home() {
   const { signIn } = useContext(AuthContext);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
   async function handleLogin(event: FormEvent) {
     event.preventDefault();
 
     let data = {
-      email: 'email@teste.com',
-      password: '1234',
+      email,
+      password,
     };
 
     await signIn(data);
@@ -35,8 +39,22 @@ export default function Home() {
 
         <div className={styles.login}>
           <form onSubmit={handleLogin}>
-            <Input type="text" placeholder="Digite seu email" />
-            <Input type="password" placeholder="Digite sua senha" />
+            <Input
+              type="text"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              placeholder="Digite seu email"
+            />
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              placeholder="Digite sua senha"
+            />
             <Button type="submit" loading={false}>
               Acessar
             </Button>
